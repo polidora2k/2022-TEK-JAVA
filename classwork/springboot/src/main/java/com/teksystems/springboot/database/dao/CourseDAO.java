229f8729e557258f258960d80cf453b77f1c505a
Ian Polidora <polidora2k@gmail.com>
2022-11-16 16:36:56 -0500

    11.16.22 Springboot Initialization Classwork.

---
+++ classwork/springboot/src/main/java/com/teksystems/springboot/database/dao/CourseDAO.java
@@ -0,0 +1,16 @@
+package com.teksystems.springboot.database.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.teksystems.springboot.database.entity.Course;
+
+@Repository
+public interface CourseDAO extends JpaRepository<Course, Integer> {
+	
+	@Query("SELECT c FROM Course c where c.name = :name")
+	public List<Course> findByName(String name);
+}
